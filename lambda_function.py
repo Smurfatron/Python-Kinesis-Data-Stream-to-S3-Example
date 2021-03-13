ba268aebc1e382649be70fd154217645a341e8b2
Smurfatron <Smurfatron@gmail.com>
2021-03-12 22:16:26 -0800

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -12,13 +12,28 @@ def lambda_handler(event, context):
     logger.debug(f"records: {len(event['records'])}")
     records = []
     for encoded_record in event['records']:
-        record_data = base64.b64decode(encoded_record['data'])
+        record_data = json.loads(base64.b64decode(encoded_record['data']))
+        summary = {
+            "email": record_data['email'],
+            "first": record_data['name']['first'],
+            "last": record_data['name']['last'],
+            "age": record_data['dob']['age'],
+            "gender": record_data['gender'],
+            "country": record_data['location']['country'],
+            "latitude": record_data['location']['coordinates']['latitude'],
+            "longitude": record_data['location']['coordinates']['longitude'],
+        }
+        data = {
+            "original_record": record_data,
+            "summary": summary
+        }
+        logger.info(f"{json.dumps(data)}")
         record = {
-            "id": encoded_record['recordId'],
-            "data": json.loads(record_data)
+            "recordId": encoded_record['recordId'],
+            "result": "Ok",
+            "data": base64.b64encode(json.dumps(data).encode('utf-8')).decode('utf-8')
         }
         records.append(record)
-        logger.info(f"{json.dumps(record)}")
     logger.info(f"records parsed: {len(records)}")
 
-    return records
+    return {"records": records}
